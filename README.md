20a1491b5b2e4d3faff22260bacdef818cc04011
anish.m.prasad <anish.m.prasad@gmail.com>
2019-07-19 13:32:06 +0530

    core update

--- README.md
+++
@@ -1,7 +0,0 @@
-# react-shakaplayer
-
-A React Port for shaka player
-
-# UNDER ACTIVE DEVELOPMENT
-
-# DONT USE FOR PRODUCTION PURPOSE
--- src/react-shakaplayer/index.js
+++ src/react-shakaplayer/index.js
@@ -18,6 +18,40 @@ import 'tippy.js/umd/index.min.js';
 
 // <script defer src="https://www.gstatic.com/cv/js/sender/v1/cast_sender.js"></script>
 
+// The compiled library, with UI.
+import '../dist/shaka-player.ui.js';
+// The compiled demo.
+import '../dist/demo.compiled.js';
+
+// The compiled library, with UI, debug mode.
+import '../dist/shaka-player.ui.debug.js';
+// The compiled demo.
+import '../dist/demo.compiled.debug.js';
+
+// Bootstrap the Shaka Player library through the Closure library.
+import '../third_party/closure/goog/base.js';
+import '../dist/deps.js';
+// This is required for goog.asserts.
+import '../lib/debug/asserts.js';
+// This file contains goog.require calls for all exported library classes.
+import '../shaka-player.uncompiled.js';
+// Enable less, the CSS pre-processor.
+import 'less/dist/less.js';
+// These are the individual parts of the demo app.
+import './js/common/asset.js';
+import './js/common/assets.js';
+import './js/demo_utils.js';
+import './js/asset_card.js';
+import './js/close_button.js';
+import './js/input.js';
+import './js/input_container.js';
+import './js/tooltip.js';
+import './js/main.js';
+import './js/front.js';
+import './js/search.js';
+import './js/custom.js';
+import './js/config.js';
+
 export default function ReactShakaPlayer(props) {
 	return <div>react shaka player</div>;
 }
