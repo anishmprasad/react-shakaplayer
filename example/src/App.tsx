a341a4a108848614012a1d0d9e607d9b5fddcdcd
Anish M Prasad <Anish.m.prasad@gmail.com>
2022-05-16 23:42:20 +0530

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import ReactDOM from 'react-dom';
+import ShakaPlayer from './Player';
 
-function App() {
+const STREAMS = [
+  {
+    name: 'Angel One MPEG-DASH',
+    src: 'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd'
+  },
+  {
+    name: 'Big Buck Bunny HLS',
+    src:
+      'https://storage.googleapis.com/shaka-demo-assets/bbb-dark-truths-hls/hls.m3u8'
+  }
+];
+
+export default function App() {
+  const [show, setShow] = React.useState(false);
+  const [chromeless, setChromeless] = React.useState(false);
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    //@ts-ignore
+    window.getShakaInst = () => ref.current;
+  }, []);
+
+  function onToggle() {
+    setShow(!show);
+  }
+
+  function onChromeless() {
+    setChromeless(!chromeless);
+  }
+
+  const [src, setSrc] = React.useState(STREAMS[0].src);
+
+  function onSelectSrc(event: { target: { value: React.SetStateAction<string>; }; }) {
+    setSrc(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div>
+      <div>
+        <button onClick={onToggle}>{show ? 'Hide' : 'Show'}</button>
+      </div>
+      <div>
+        <input type="checkbox" onChange={onChromeless} /> Chromeless
+      </div>
+      <div>
+        <select value={src} onChange={onSelectSrc}>
+          {STREAMS.map(stream => (
+            <option value={stream.src}>{stream.name}</option>
+          ))}
+        </select>
+      </div>
+      {show && (
+        <ShakaPlayer ref={ref} autoPlay src={src} chromeless={chromeless} />
+      )}
     </div>
   );
 }
-
-export default App;
